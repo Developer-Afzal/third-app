77ca862117052398b95b3192e32b07198ce98985
Developer-Afzal <98870193+Developer-Afzal@users.noreply.github.com>
2022-08-09 00:57:13 +0530

    Update ContactForm.js
    upadte from vailidation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import emailjs from 'emailjs-com';
-import {Link} from "react-router-dom"
-
+import {useForm} from "react-hook-form";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
  const ContactForm = () => {
-    function Sendemail(e){
-      e.preventDefault();
-      emailjs.sendForm('service_ree3gub','template_bd42y5n',e.target,'icTbL0LKXtBKO1wl0').then(res=>{
-      console.log(res);
-      }).catch(err=> console.log(err));
-      
-     
-     
+  const navigate= useNavigate();
+  const [userinfo, setuserinfo] = useState( );
+  const { register, handleSubmit, formState:{errors}} = useForm();
+    function onSubmit(data){
+      // data.preventDefault();
+        setuserinfo("We have Received your request");
+        navigate('/thanks'); 
+        console.log(data);
     }
 
   return (
     <>
-    <form>
+    {/* <pre style={{color:'red'}}>{JSON.stringify(userinfo, undefined, 3)}</pre> */}
+    <form onSubmit={handleSubmit(onSubmit)}>
     <div className="col-md-8 mx-auto mt-2">
-      <label id="exampleFormControlInput1" className="form-label">Enter Your Contact Number</label>
-      <input className="form-control form-control-sm" name="name" />
+      <label>Enter Your Full Name</label>
+      <input className="form-control form-control-sm" type="text" name="name" {...register("name",{required:"Name is required", maxLength: { value:25, message:"Your name is soo long"},pattern:{value:/^[+-]?\D*(?:[.,]\d*)?$/, message:"Please enter only Alphabet"}})} />
+      <p>{errors.name?.message}</p>
       </div>
       <div className="col-md-8 mx-auto mt-2">
-      <label id="exampleFormControlInput1" className="form-label">Enter Your Contact Number</label>
-      <input className="form-control form-control-sm" name="contact"/>
+      <label>Enter Your Contact Number</label>
+      <input className="form-control form-control-sm" name="contact" {...register("contact",{required:"Contact is Required",minLength:{value:10, message:"Please Enter Maximum 10 Digits"}, maxLength:{value:10, message:"Please enter vaild contact No"},pattern:{value:/^[+-]?\d*(?:[.,]\d*)?$/, message:"Please enter only Number"}})} />
+      <p>{errors.contact?.message}</p>
       </div>
       <div className="col-md-8 mx-auto mt-2">
-      <label id="exampleFormControlInput1" className="form-label">Enter Your Email Address</label>
-      <input className="form-control form-control-sm" name="user_email" />
+      <label>Enter Your Email Address</label>
+      <input className="form-control form-control-sm" type="email" name="user_email" {...register("email",{required: "email is required" })}  />
+      <p>{errors.email?.message}</p>
       </div>
-      <div className="col-md-4 mx-auto text-center mt-4"><button className='btn btn-info' ><Link to="/thanks" className='link' >Submit</Link></button></div>
+      <div className="col-md-4 mx-auto text-center mt-4"><button className='btn btn-info'>Submit</button></div>
       </form>
     </>
   );
